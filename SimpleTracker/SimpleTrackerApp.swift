b074e972e95017954101f340c6256ec3b512471d
Alex <foxwalkin@protonmail.com>
2024-10-15 13:48:54 -0500

    Working settings menu!

--- SimpleTracker/SimpleTrackerApp.swift
+++ SimpleTracker/SimpleTrackerApp.swift
@@ -10,19 +10,24 @@ import SwiftUI
 @main
 struct SimpleTrackerApp: App {
     @State var viewModel = ViewModel()
-        
+    
     func resetTracker() {
         viewModel.resetBosses()
         viewModel.resetItems()
     }
 
-    private var settingsMenu: some View {
-        ForEach(viewModel.seedOptions, id: \.key) { seedOption in
-            Menu(seedOption.title) {
-                ForEach(Array(seedOption.options.enumerated()), id: \.offset) { index, option in
-                    Button(option, action: {
-                        seedOption.update(index)
-                    })
+    struct settingsMenu: View {
+        @Environment(ViewModel.self) private var viewModel
+        
+        var body: some View {
+            ForEach(viewModel.seedOptions, id: \.key) { seedOption in
+                Picker(seedOption.title, selection: Binding(
+                    get: { seedOption.selection },
+                    set: { seedOption.update($0) })) {
+                    ForEach(Array(seedOption.options.enumerated()), id: \.offset) { index, option in
+                        Text(option)
+                            .tag(index) // Use 'index' as the tag if you're selecting by index
+                    }
                 }
             }
         }
@@ -41,14 +46,15 @@ struct SimpleTrackerApp: App {
                 }
                 .keyboardShortcut("R", modifiers: [.command])
             }
+            //                CommandMenu("View") {
+            //                    Button(appSettings.showSeedName ? "Hide Seed Name" : "Show Seed Name") {
+            //                        appSettings.showSeedName.toggle()
+            //                        AppSettings.defaults.set(appSettings.showSeedName, forKey: "showSeedName")
+            //                    }
+            //                }
             CommandMenu("Settings") {
-//                CommandMenu("View") {
-//                    Button(appSettings.showSeedName ? "Hide Seed Name" : "Show Seed Name") {
-//                        appSettings.showSeedName.toggle()
-//                        AppSettings.defaults.set(appSettings.showSeedName, forKey: "showSeedName")
-//                    }
-//                }
-                settingsMenu
+                settingsMenu()
+                    .environment(viewModel)
             }
         }
     }
