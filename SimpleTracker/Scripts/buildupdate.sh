7aed8e633ca8af9cdb5814d2300ba50205c320df
Alex <foxwalkin@protonmail.com>
2024-10-17 18:13:01 -0500

    fixed automatic build number management

--- SimpleTracker/Scripts/buildupdate.sh
+++ SimpleTracker/Scripts/buildupdate.sh
@@ -8,12 +8,14 @@
 # Copyright (C) 2024 Warpixel
 #
 
-cd "$SRCROOT/$PRODUCT_NAME"
+cd "$SRCROOT/$PROJECT_NAME"
+
+awk -F "=" '/BUILD_NUMBER/ {print $2}' Config.xcconfig | tr -d ' '
 
 old_build_number=$(awk -F "=" '/BUILD_NUMBER/ {print $2}' Config.xcconfig | tr -d ' ')
-new_build_numer=$((old_build_number + 1))
+new_build_number=$((old_build_number + 1))
 
 # update BUILD_NUMBER with new build number while backing up Config.xcconfig
 # then delete the backup after it completes
-sed -i -e "/BUILD_NUMBER =/ s/= .*/= $new_build_number/" Config.xcconfig
-rm -f Config.xcconfig-e
+sed -i -e "/BUILD_NUMBER =/ s/= .*/= $new_build_number/" Config.xcconfig >> log.txt
+rm -f Config.xcconfig-e >> log.txt
